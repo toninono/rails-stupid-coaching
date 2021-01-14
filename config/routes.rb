6dd332ac23b216af23276d0e4a364bd7b486eaaa
Antoine Legoix <antoine.legoix@gmail.com>
2021-01-14 18:39:13 +0100

    stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'answer', to: 'questions#answer'
+  get 'ask', to: 'questions#ask'
+  root 'questions#ask'
+
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
